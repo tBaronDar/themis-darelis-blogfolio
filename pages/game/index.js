3f52fe4f1226c8816e9c25b09c978f3faab31362
tBaronDar <thedarelis@gmail.com>
2024-06-10 17:10:14 +0300

    game itch.io

--- pages/game/index.js
+++ pages/game/index.js
@@ -1,5 +1,15 @@
 function GamePage() {
-	return <iframe src="/game/index.html"></iframe>;
+	return (
+		<div>
+			<iframe
+				frameborder="0"
+				src="https://itch.io/embed/2761298"
+				width="552"
+				height="167">
+				<a href="https://thedar1.itch.io/quiqui-run">Quiqui Run by theDar1</a>
+			</iframe>
+		</div>
+	);
 }
 
 export default GamePage;
