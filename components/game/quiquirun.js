649ae6ac573df39847cad801d7cb3d926362892b
tBaronDar <thedarelis@gmail.com>
2024-06-14 15:03:41 +0300

    css quickfix1

--- components/game/quiquirun.js
+++ components/game/quiquirun.js
@@ -17,9 +17,10 @@ function Quiquirun() {
       </p>
       {isClient && (
         <iframe
+          className={classes.itchio}
           frameborder="0"
           src="https://itch.io/embed/2761298"
-          width="552"
+          width="360"
           height="167"
         >
           <a href="https://thedar1.itch.io/quiqui-run">Quiqui Run by theDar1</a>
